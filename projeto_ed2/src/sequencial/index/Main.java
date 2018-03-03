5c66d3a113a903433972f13e0596896c27c32780
wesleyat <ricky.a.04@gmail.com>
2018-03-03 01:40:14 -0300

    Correções na captura das informações do aluno (no hora de adicionar um novo registro).

--- projeto_ed2/src/sequencial/index/Main.java
+++ projeto_ed2/src/sequencial/index/Main.java
@@ -8,6 +8,10 @@ import comum.Aluno;
 public class Main {
 
 	private static final String DB_FILE_NAME = "alunos.db";
+	private static final int MAX_NOME_LEN     = 39, // 40 - 1 caracteres
+							 MAX_ENDERECO_LEN = 49, // 50 - 1 caracteres
+							 MAX_TELEFONE_LEN = 9,  // 10 - 1 caracteres
+							 MAX_EMAIL_LEN    = 44; // 45 - 1 caracteres
 	private static OrganizadorSequencial org;
 	private static String dbPath,
 						  dbFullPath;
@@ -62,17 +66,21 @@ public class Main {
 				System.out.print( "Curso: " );
 				short curso = scan.nextShort();
 				System.out.print( "Nome (até 40 caracteres): " );
+				scan.nextLine();
 				String nome = scan.nextLine();
-				nome = nome.substring( 0, 39 );
+				nome = nome.substring( 0, nome.length() > MAX_NOME_LEN ? MAX_NOME_LEN : nome.length() );
 				System.out.print( "Endereço (até 50 caracteres): " );
+				//scan.nextLine();
 				String endereco = scan.nextLine();
-				endereco = endereco.substring( 0, 49 );
+				endereco = endereco.substring( 0, endereco.length() > MAX_ENDERECO_LEN ? MAX_ENDERECO_LEN : endereco.length() );
 				System.out.print( "Telefone (até 10 caracteres): " );
+				//scan.nextLine();
 				String telefone = scan.nextLine();
-				telefone = telefone.substring( 0, 9 );
+				telefone = telefone.substring( 0, telefone.length() > MAX_TELEFONE_LEN ? MAX_TELEFONE_LEN : telefone.length() );
 				System.out.print( "E-mail (até 45 caracteres): " );
+				//scan.nextLine();
 				String email = scan.nextLine();
-				email = email.substring( 0, 44 );
+				email = email.substring( 0, email.length() > MAX_EMAIL_LEN ? MAX_EMAIL_LEN : email.length() );
 				
 				aluno = new Aluno( matricula, curso, nome, endereco );
 				aluno.setEmail( email );
