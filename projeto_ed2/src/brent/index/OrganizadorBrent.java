d2e1c6ffce48a031131f8874198f502b38ed6b5b
wesleyat <ricky.a.04@gmail.com>
2018-03-04 23:36:01 -0300

    Novo método (incompleto) getAlunoPosition.

--- projeto_ed2/src/brent/index/OrganizadorBrent.java
+++ projeto_ed2/src/brent/index/OrganizadorBrent.java
@@ -1,6 +1,7 @@
 package brent.index;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
@@ -32,19 +33,35 @@ public class OrganizadorBrent implements IFileOrganizer {
 	}
 
 	@Override
-	public void addAluno(Aluno p) {
+	public void addAluno( Aluno p ) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public Aluno getAluno(long matric) {
-		// TODO Auto-generated method stub
+	public Aluno getAluno( long matric ) {
+		
+		Aluno aluno = null;
+		
+		
+		
+		byte[] nome = new byte[80];
+		byte[] endereco = new byte[100];
+		byte[] telefone = new byte[20];
+		byte[] email = new byte[90];
+		
+		long mat = buffer.getLong();
+		short curso = buffer.getShort();
+		buffer.get( nome );
+		buffer.get( endereco );
+		buffer.get( telefone );
+		buffer.get( email );
+		
 		return null;
 	}
 
 	@Override
-	public Aluno delAluno(long matric) {
+	public Aluno delAluno( long matric ) {
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -52,4 +69,28 @@ public class OrganizadorBrent implements IFileOrganizer {
 	private int hash( long chave ) { return ( int )chave % NUM_OF_REC; }
 	
 	private int inc( long chave ) { return ( ( int )chave % ( NUM_OF_REC -2 ) ) +1; }
+	
+	private int getAlunoPosition( int matric ) {
+		
+		int position = hash( matric );
+		
+		try {
+			rf = new RandomAccessFile( file, "r" );
+			channel = rf.getChannel();
+			buffer.position( 0 );
+			
+			if( channel.read( buffer, position ) > -1 ) {
+				
+				long mat = buffer.getLong( 0 );
+				
+				if( matric == mat )
+					return position;
+				
+				// TODO : implementar a busca pelo menor esforço
+			}
+		}
+		catch ( IOException e ) { e.printStackTrace();	}
+		
+		return -1;
+	}
 }
