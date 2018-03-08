aa5674dbdd31f2d5cb53109e99923372e9fd0309
wesleyat <ricky.a.04@gmail.com>
2018-03-08 19:59:30 -0300

    Reorganização da classe: remove procedimentos desnecessários e reorganiza o código.

--- projeto_ed2/src/comum/Aluno.java
+++ projeto_ed2/src/comum/Aluno.java
@@ -8,31 +8,18 @@ public class Aluno implements Comparable<Object> {
 
 	private long matric;
 	private short curso;
-	private static final int MAX_NOME_LEN     = 80,
-			 				 MAX_ENDERECO_LEN = 100,
-			 				 MAX_TELEFONE_LEN = 20,
-			 				 MAX_EMAIL_LEN    = 90;
 	private String nome,
 				   endereco,
 				   telefone,
 				   email;
-				   
-	public Aluno() {}
+
 	
 	public Aluno( long matricula, String nome, String endereco, short curso ) {
 		
-		this.matric = matricula;
+		matric = matricula;
+		this.nome = nome;
+		this.endereco = endereco;
 		this.curso = curso;
-		
-		if( nome.length() > MAX_NOME_LEN )
-			this.nome = nome.substring( 0, MAX_NOME_LEN -1 );
-		else
-			this.nome = nome + new String( new char[MAX_NOME_LEN - nome.length()] );
-		
-		if( endereco.length() > MAX_ENDERECO_LEN )
-			this.endereco = endereco.substring( 0, MAX_ENDERECO_LEN -1 );
-		else
-			this.endereco = endereco + new String( new char[MAX_ENDERECO_LEN - endereco.length()] );
 	}
 	
 	public long getMatricula() { return matric; }
@@ -43,47 +30,24 @@ public class Aluno implements Comparable<Object> {
 	
 	public String getEndereco() { return endereco; }
 	
-	public String getTelefone() { return telefone.isEmpty() ? "" : telefone; }
+	public String getTelefone() { return telefone; }
 	
-	public String getEmail() { return email.isEmpty() ? "" : email; }
+	public String getEmail() { return email; }
 	
 	public void setCurso( short curso ) { this.curso = curso; }
 	
-	public void setNome( String nome ) {
-		
-		if( nome.length() > MAX_NOME_LEN )
-			this.nome = nome.substring( 0, MAX_NOME_LEN -1 );
-		else
-			this.nome = nome + new String( new char[MAX_NOME_LEN - nome.length()] ); 
-	}
+	public void setNome( String nome ) { this.nome = nome; }
 	
-	public void setEndereco( String endereco ) { 
-		
-		if( endereco.length() > MAX_ENDERECO_LEN )
-			this.endereco = endereco.substring( 0, MAX_ENDERECO_LEN -1 );
-		else
-			this.endereco = endereco + new String( new char[MAX_ENDERECO_LEN - endereco.length()] ); 
-	}
+	public void setEndereco( String endereco ) { this.endereco = endereco; }
 	
-	public void setTelefone( String telefone ) {
-		
-		if( telefone.length() > MAX_TELEFONE_LEN )
-			this.telefone = telefone.substring( 0, MAX_TELEFONE_LEN -1 );
-		else
-			this.telefone = telefone + new String( new char[MAX_TELEFONE_LEN - telefone.length()] ); 
-	}
+	public void setTelefone( String telefone ) { this.telefone = telefone; }
 	
-	public void setEmail( String email ) { 
-		
-		if( email.length() > MAX_EMAIL_LEN )
-			this.email = email.substring( 0, MAX_EMAIL_LEN -1 );
-		else
-			this.email = email + new String( new char[MAX_EMAIL_LEN - email.length()] );
-	}
+	public void setEmail( String email ) { this.email = email; }
 
 	public boolean equals( Object outro ) {
 		
-		if( outro instanceof Aluno ) return matric == ( ( Aluno )outro ).matric;
+		if( outro instanceof Aluno )
+			return matric == ( ( Aluno )outro ).matric;
 		
 		return false;
 	}
@@ -91,7 +55,8 @@ public class Aluno implements Comparable<Object> {
 	@Override
 	public int compareTo( Object outro ) throws IllegalArgumentException {
 		
-		if( !equals( outro ) ) throw new IllegalArgumentException();
+		if( !equals( outro ) )
+			throw new IllegalArgumentException();
 		
 		if( matric < ( ( Aluno )outro ).matric ) return -1;
 		
